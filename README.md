86e5ef3d39e1416a7282fdd3f6bb40091cc9d8a0
Brittany Storoz <brittanystoroz@gmail.com>
2016-09-14 17:19:48 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Expense Tracker
+
+A simple API for tracking expenses.
+
+## Setup
+
+```
+npm install
+```
+
+## Running
+
+```
+npm start
+```
+
+Go visit `http://localhost:3000`.
+
+## Endpoints
+
+* `GET /expenses` returns a all of the expenses available.
+* `GET /expenses/:id` returns a given expense from the API.
+* `POST /expenses` creates a new expense.
+* `PUT /expenses/:id` updates the content of the expense.
+* `DELETE /expenses/:id` deletes a expense.
---
+++ public/script.js
@@ -0,0 +1,52 @@
+const loadExpenses = () => {
+  $.ajax({
+    method: 'GET',
+    dataType: 'json',
+    url: '/expenses',
+    success: function (data) {
+      console.log('Expenses returned successfully.', data);
+    },
+    error: function (error) {
+      console.error('There was some kind of error.', error);
+    }
+  });
+};
+
+
+$('#submit-expense').on('submit', (e) => {
+  e.preventDefault();
+
+  let data = {};
+  $('.form-field').each((index, formField) => {
+    data[$(formField).attr('name')] = $(formField).val();
+  });
+
+  $.ajax({
+    method: 'POST',
+    dataType: 'json',
+    url: '/expenses',
+    data: data,
+    success: function (data) {
+      console.log('Expense was sent successfully.', data);
+      loadExpenses();
+    },
+    error: function (error) {
+      console.error('There was some kind of error.', error);
+    }
+  });
+});
+
+
+// Highlight expenses that match the selected category
+$('#highlight-category').on('change', (e) => {
+  $('#expenses-data').find('tr').removeClass('highlighted'); // remove any existing highlights
+  const selectedCategory = $(e.currentTarget).val().toLowerCase();
+  const matches = $('#expenses-data').find(`tr.${selectedCategory}`);
+  matches.each((index, match) => {
+    $(match).addClass('highlighted');
+  });
+});
+
+$(document).ready(() => {
+  loadExpenses();
+});
